9259d51506c3b2fe73b66f107a7b6475fcf21a94
whythatt <zakrogo513@gmail.com>
2024-02-09 19:36:25 +0300

    add app in django

--- django_app/AnimeOngoing/AnimeOngoing/settings.py
+++ django_app/AnimeOngoing/AnimeOngoing/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "anime",
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,14 @@ WSGI_APPLICATION = "AnimeOngoing.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "anime_db",
+        "USER": "anime_user",
+        "PASSWORD": "whyzakuchiha",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
